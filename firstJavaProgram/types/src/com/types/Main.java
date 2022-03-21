2a47fa80850eafd4deca635a89634bebb49c29b8
sharonjoykarimi <66792605+sharonjoykarimi@users.noreply.github.com>
2022-03-21 16:03:15 +0300

    Add files via upload

---
+++ firstJavaProgram/types/src/com/types/Main.java
@@ -0,0 +1,24 @@
+package com.types;
+
+public class Main {
+
+    public static void main(String[] args) {
+        byte age = 109;
+
+        short people = 890;
+
+        int votes = 32000;
+        long count = 10000000;
+        double priceTag= 10.99;
+        char letter ='A';
+        boolean isFunny = true;
+
+        System.out.println(age);
+        System.out.println(people);
+        System.out.println(votes);
+        System.out.println(count);
+        System.out.println(priceTag);
+        System.out.println(letter);
+        System.out.println(isFunny);
+    }
+}
