2235b463138f9cfc37a56f758a6de938ce269c7e
sharonjoykarimi <66792605+sharonjoykarimi@users.noreply.github.com>
2022-04-04 11:10:07 +0300

    Add files via upload

---
+++ strings/src/com/codewithjoy/Main.java
@@ -0,0 +1,29 @@
+package com.codewithjoy;
+
+public class Main {
+
+    public static  void main(String[] args) {
+
+        String message="Hello Alicia"+ "???";
+        System.out.println(message);
+        System.out.println(message.indexOf("e"));
+        System.out.println(message.indexOf("car"));
+
+        System.out.println(message.endsWith("?"));
+
+
+        System.out.println(message.startsWith("j"));
+
+
+        System.out.println(message.length());
+
+        System.out.println(message.toUpperCase());
+
+        System.out.println(message.toLowerCase());
+
+        System.out.println("THANKYOU");
+
+
+
+    }
+}
