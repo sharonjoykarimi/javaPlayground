938bb1856f033c0d4542d69f761614d9563a0072
sharonjoykarimi <66792605+sharonjoykarimi@users.noreply.github.com>
2022-04-04 11:07:26 +0300

    Add files via upload

---
+++ refrence/src/com/codewithjoy/Main.java
@@ -0,0 +1,15 @@
+package com.codewithjoy;
+
+
+import java.util.Date;
+
+
+public class Main {
+
+    public static void main(String[] args) {
+        Date no= new Date();
+        System.out.println(no);
+
+
+    }
+}
