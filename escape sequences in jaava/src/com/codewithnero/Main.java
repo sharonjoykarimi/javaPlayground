e53ce9b742b4057fe8ffb51c6e747483449ea8cd
sharonjoykarimi <66792605+sharonjoykarimi@users.noreply.github.com>
2022-04-04 11:13:14 +0300

    Add files via upload

---
+++ escape sequences in jaava/src/com/codewithnero/Main.java
@@ -0,0 +1,13 @@
+package com.codewithnero;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+
+        // c:\linux\...
+
+        System.out.println("hello \"World\"");
+        System.out.println("// c:\\linux\\...");
+    }
+}
