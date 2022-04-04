d295fdbc7f90d71c6ab0a7b52c5769f35e5f80cd
sharonjoykarimi <66792605+sharonjoykarimi@users.noreply.github.com>
2022-04-04 11:14:48 +0300

    Add files via upload

---
+++ arrays/src/com/codewithjoy/array.java
@@ -0,0 +1,15 @@
+
+package com.codewithjoy;
+import java.util.Arrays;
+public  class main {
+
+    public static void main(String[] args) {
+        int[][] numbers ={{1,2,3},{ 4,5,6}};
+        numbers[0][0]=1;
+        System.out.println(Arrays.toString(numbers));
+         final float PI=3.14F;
+        System.out.println("main.main");
+
+
+    }
+}
---
+++ arrays/src/com/codewithjoy/main.java
@@ -0,0 +1,16 @@
+package com.codewithjoy;
+
+import java.util.Arrays;
+
+public class main {
+
+    public static void main(String[] args) {
+        int[] numbers = {2, 3, 1, 4};
+
+        System.out.println(numbers.length);
+
+        System.out.println(Arrays.toString(numbers));
+
+         final float PI=3.14F;
+    }
+}
