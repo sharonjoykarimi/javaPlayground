ea55b52edbef9b1f2d9e24842f47aeee5a8d898f
sharonjoykarimi <66792605+sharonjoykarimi@users.noreply.github.com>
2022-04-06 10:24:06 +0300

    Add files via upload

---
+++ Comparison Operators/src/com/codewithjava/Main.java
@@ -0,0 +1,15 @@
+package com.codewithjava;
+
+public class Main {
+    //this will print true because 12is equal 12
+    public static void main(String[] args) {
+        int a = 12;
+        int b = 12;
+        System.out.println(a == b);
+        // it will print false because 12 is not equals 11
+        int c = 12;
+        int d = 11;
+        System.out.println(c == d);
+
+    }
+}
