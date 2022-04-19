88043415a76c3a5ead4560b26d58a307142a0448
sharonjoykarimi <66792605+sharonjoykarimi@users.noreply.github.com>
2022-04-19 12:32:30 +0300

    calculator

---
+++ calculator/src/com/codewithjava/Main.java
@@ -0,0 +1,33 @@
+package com.codewithjava;
+
+import java.text.NumberFormat;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        final byte MONTHS_IN_YEAR = 12;
+        final byte PERCENT = 100;
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Principal: ");
+        int Principal = scanner.nextInt();
+
+        System.out.println("Annual Interest Rate: ");
+        float AnnualInterest = scanner.nextFloat();
+        float monthlyInterest = AnnualInterest / PERCENT / MONTHS_IN_YEAR;
+
+        System.out.println("period(years); ");
+        byte years = scanner.nextByte();
+        int numberOfPayment = years * MONTHS_IN_YEAR;
+
+        double mortgage = Principal
+                * (monthlyInterest * Math.pow(1 + monthlyInterest, numberOfPayment));
+               /(Math.pow(1+monthlyInterest,numberOfPayment) -1);
+
+        String mortgageFormatted = NumberFormat.getCurrencyInstance().format(mortgage) ;
+        System.out.println("Mortgage: " + mortgageFormatted);
+
+
+    }
+}
