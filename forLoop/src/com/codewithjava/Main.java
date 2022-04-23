2e21c02bf5e7f30c544e8d80cfa4b24f71140316
sharonjoykarimi <66792605+sharonjoykarimi@users.noreply.github.com>
2022-04-23 21:29:31 +0300

    forLoops

---
+++ forLoop/src/com/codewithjava/Main.java
@@ -0,0 +1,11 @@
+package com.codewithjava;
+
+public class Main {
+
+    public static void main(String[] args) {
+        // this is for loops
+	  for (int i= 1; i <=5 ; i++)
+          System.out.println("Hello World" + i);
+
+    }
+}
